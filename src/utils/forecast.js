47128f6c720299137f6752b84e42382465aecc57
Mohammed ALDAHER <mohaldaher29@gmail.com>
2022-07-24 19:34:33 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require("postman-request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=cf6c9267d18a4a082f63778ada6fba72&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=m";
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        response.body.current.weather_descriptions[0] +
+          ". It is currently " +
+          response.body.current.temperature +
+          " degress out."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
