0258950246b9e629b21ff851c1063ac3e0af44ab
Mohammed ALDAHER <mohaldaher29@gmail.com>
2022-07-24 20:47:38 +0300

    modified forecast file

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,25 +1,27 @@
-const request = require("postman-request");
+const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
   const url =
-    "http://api.weatherstack.com/current?access_key=cf6c9267d18a4a082f63778ada6fba72&query=" +
+    'http://api.weatherstack.com/current?access_key=cf6c9267d18a4a082f63778ada6fba72&query=' +
     latitude +
-    "," +
+    ',' +
     longitude +
-    "&units=m";
+    '&units=m';
 
   request({ url: url, json: true }, (error, response) => {
     if (error) {
-      callback("Unable to connect to weather service!", undefined);
+      callback('Unable to connect to weather service!', undefined);
     } else if (response.body.error) {
-      callback("Unable to find location", undefined);
+      callback('Unable to find location', undefined);
     } else {
       callback(
         undefined,
         response.body.current.weather_descriptions[0] +
-          ". It is currently " +
+          '. It is currently ' +
           response.body.current.temperature +
-          " degress out."
+          ' degress out, and the wind speed is ' +
+          response.body.current.wind_speed +
+          ' km/h'
       );
     }
   });
